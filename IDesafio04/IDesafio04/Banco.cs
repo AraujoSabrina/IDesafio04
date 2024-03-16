7b2ca1519c3ec94cff718c8bcade61e6db6cfc38
Sabrina <141427192+AraujoSabrina@users.noreply.github.com>
2024-03-16 07:17:49 -0300

    Add files via upload

---
+++ IDesafio04/IDesafio04/Banco.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace IDesafio04
+{
+    public class Banco
+    {
+        List<Usuario> usario = new List<Usuario>();
+        public void adicionarUsuario(Usuario user)
+        {
+            usario.Add(user);
+        }
+       
+
+        public double depositar(int id, int deposito)
+        {
+           for (int i = 0; i < usario.Count; i++)
+            {
+                if (id == usario[i].getId())
+                this.Valor += deposito;
+
+
+            }
+
+            return Valor;
+        }
+        public double Valor { get; private set; } = 200;
+        public double getConta(int id)
+        {
+            for (int i = 0; i < usario.Count; i++)
+            {
+                if (id == usario[i].getId())
+                    return this.Valor;
+            }
+            return 0;
+        }
+        public double sacar(int id, int saque)
+        {
+            for (int i = 0; i < usario.Count; i++)
+            {
+                if (id == usario[i].getId())
+                    this.Valor -= saque;
+                    return this.Valor;
+
+            }
+            return 0;
+        }
+
+    }
+
+}
---
+++ IDesafio04/IDesafio04/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDesafio04
+{
+
+    public class Usuario
+    {
+        public int Id { get; private set; }
+        public Usuario(int id) {
+            this.Id = id;
+        }
+      
+       public int getId() { 
+            return this.Id;
+       }
+    }
+}
