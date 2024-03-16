a9ca783ff7504d0d2cd5818e617029030bed9cea
Sabrina <141427192+AraujoSabrina@users.noreply.github.com>
2024-03-16 09:05:12 -0300

    Update Program.cs

--- IDesafio04/IDesafio04/Program.cs
+++ IDesafio04/IDesafio04/Program.cs
@@ -1,14 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 
 using IDesafio04;
 
 Banco banco = new Banco();
 
-Console.WriteLine($"Antes do deposito: {banco.getConta(2)}");
-banco.adicionarUsuario(Usuario user(1));
+Usuario usuario = new Usuario(1);
+
+Console.WriteLine($"Antes do deposito: {banco.getConta(1)}");
+banco.adicionarUsuario(usuario);
 
 banco.depositar(1, 3);
-Console.WriteLine($"Depois do deposito: {banco.getConta(2)}");
+Console.WriteLine($"Depois do deposito: {banco.getConta(1)}");
 
-banco.sacar(1, 10);
-Console.WriteLine($"Depois do saque: {banco.getConta(2)}");
+banco.sacar(1, 100000);
+Console.WriteLine($"Depois do saque: {banco.getConta(1)}");
